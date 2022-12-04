883756f57fec57eaf6c55a01c7b3fddf8188c35b
Gustavo Casagrande Borges <borgesgustavo360@outlook.com>
2022-12-04 15:11:04 -0400

    Manipulação básica de arrays

---
+++ 07_manipulandoArrays.php
@@ -0,0 +1,38 @@
+<?php
+
+////Criando arrays utilizando colchetes
+//$names = [
+//    'Gustavo', 'Ian', 'Marcos', 'Wata',
+//];
+
+//$nameOne = 'Gustavo';
+//$nameTwo = 'Ian';
+//$nameThree = 'Marcos';
+//$nameFour = 'Wata';
+
+////Criando arrays utilizando variáveis:
+
+//// Criando o arrays e definindo os índices como sendo as variáveis
+/*
+$names = [
+    $nameOne,
+    $nameTwo,
+    $nameThree,
+    $nameFour
+];
+var_dumb($names)
+*/
+
+//// Criando o array utilizando método compact
+//$names = compact('nameOne', 'nameTwo', 'nameThree', 'nameFour');
+//var_dump($names);
+//print("<br>");
+
+////Verificando se um elemento é ou não um array
+//var_dump(is_array($nameOne));
+//print("<br>");
+
+////Verificando existência de um elemento em uma lista
+//var_dump(in_array('Gustavo', $names));
+//print("<br>");
+//var_dump(in_array('Banana', $names));
