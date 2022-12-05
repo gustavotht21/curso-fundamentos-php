f4f2493a9e7127acaa7c1bd4217590eefc64e554
Gustavo Casagrande Borges <borgesgustavo360@outlook.com>
2022-12-04 21:06:36 -0400

    Laços de repetição

---
+++ 05 Loops de repeticao/16_loops.php
@@ -0,0 +1,38 @@
+<?php
+
+$names = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
+//// For
+//print ('<hr>');
+//for ($i=0; $i<10; $i++) {
+//    print ("{$i} <br>") ;
+//}
+//print ('<hr>');
+//
+//for ($i=0; $i<count($names); $i++) {
+//    print ("{$names[$i]} <br>");
+//}
+//print ('<hr>');
+
+//// Foreach
+//print ('<hr>');
+//foreach ($names as $key => $name) {
+//    print ("{$key} => {$name} <br>");
+//}
+//print ('<hr>');
+
+//// While
+//print ('<hr>');
+//$index = 0;
+//while ($index < count($names)) {
+//    print ("{$names[$index]} <br>");
+//    $index++;
+//}
+//print ('<hr>');
+
+//// Do While
+//$index = 1;
+//do {
+//    print ("{$index} <br>");
+//    $index++;
+//} while ($index <= 10);
