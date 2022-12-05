828f871d1beb2fd9b95d0892937e244f48214993
Gustavo Casagrande Borges <borgesgustavo360@outlook.com>
2022-12-05 14:44:41 -0400

    Formulário e aquisição de dados inseridos pelo usuário

---
+++ 09 Entrada de dados/forms/script.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * Validação simples de dados
+if ($_REQUEST['nome'] == '' && strlen($_REQUEST['nome'] <= 5)) {
+    print ('O campo nome não é válido');
+    return; //die(); || exit();
+} else {
+    print ("Nome: {$_REQUEST['nome']}");
+    print ('<hr>');
+}
+// REQUEST: Universal. Dados Get e Post. Um pouco mais lento.
+print ("Email: {$_REQUEST['email']}");
+print ('<hr>');
+print ("Password: {$_REQUEST['password']}");
+print ('<hr>');
+$concordou = isset($_REQUEST['concordo']) && $_REQUEST['concordo'] != '' ? 'Sim' : 'Não';
+print ("Concordo: {$concordou}");
+print ('<hr>');
+print ("Sexo: {$_REQUEST['sexo']}");
+print ('<hr>');
+print ("Estado: {$_REQUEST['estado']}");
+print ('<hr>');
+print ("Descricao: {$_REQUEST['descricao']}");
+print ('<hr>');
+*/
+
+/*
+ * POST:
+print ("Nome: {$_POST['nome']}");
+print ('<hr>');
+print ("Email: {$_POST['email']}");
+print ('<hr>');
+print ("Password: {$_POST['password']}");
+print ('<hr>');
+$concordou = isset($_POST['concordo']) && $_POST['concordo'] != '' ? 'Sim' : 'Não';
+print ("Concordo: {$concordou}");
+print ('<hr>');
+print ("Sexo: {$_POST['sexo']}");
+print ('<hr>');
+print ("Estado: {$_POST['estado']}");
+print ('<hr>');
+print ("Descricao: {$_POST['descricao']}");
+print ('<hr>');
+*/
+
